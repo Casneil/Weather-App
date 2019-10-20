f77d267b53eb54ab74da6a29adbed8b24e91513d
Casneil Simpson <casneil.simpson@gmail.com>
2019-10-20 06:08:28 +0200

    Updates

---
+++ precache-manifest.359f5e1092732f60c354c4c5f4efa071.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40448deb22b5a6897eac0c1ed14245a3",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "f2c9fb9b17b553fe9d95",
+    "url": "/Weather-App/static/css/main.bd4db5be.chunk.css"
+  },
+  {
+    "revision": "370d587bcf3dec839010",
+    "url": "/Weather-App/static/js/2.244b9e6c.chunk.js"
+  },
+  {
+    "revision": "f2c9fb9b17b553fe9d95",
+    "url": "/Weather-App/static/js/main.0342a717.chunk.js"
+  },
+  {
+    "revision": "377e859b4bc60009a76b",
+    "url": "/Weather-App/static/js/runtime~main.ec7003ae.js"
+  },
+  {
+    "revision": "74120c3c4700788ca920924e5fa43ff6",
+    "url": "/Weather-App/static/media/Berlin.74120c3c.jpg"
+  },
+  {
+    "revision": "6dbf85189648af846c60f0842457dc99",
+    "url": "/Weather-App/static/media/Jamaica.6dbf8518.jpg"
+  },
+  {
+    "revision": "3aa788513ab56ccefc17723c7e546e3e",
+    "url": "/Weather-App/static/media/path.3aa78851.svg"
+  },
+  {
+    "revision": "ea8b6ac2148b247bc2c39080ca63b9db",
+    "url": "/Weather-App/static/media/snow.ea8b6ac2.svg"
+  },
+  {
+    "revision": "e5a3e3b639e646db19c38d5041a15235",
+    "url": "/Weather-App/static/media/sunny.e5a3e3b6.svg"
+  }
+]);
